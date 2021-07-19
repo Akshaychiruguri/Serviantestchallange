1dd0d3bd961d0321204b4217f9e154c9d35eb0b6
Akshaychiruguri <73805429+Akshaychiruguri@users.noreply.github.com>
2021-07-19 11:51:01 +1000

    Update variables.tf

--- env/dev/us-east-1/variables.tf
+++ env/dev/us-east-1/variables.tf
@@ -2,7 +2,7 @@
 variable "region" {
   description = "The region of the AWS service."
   type        = string
-  default     = "us-east-1"
+  default     = "ap-southeast-2"
 }
 
 variable "profile" {
