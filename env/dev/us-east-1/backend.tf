80dfbf11969867ef895ff858cf92d370843ab604
Akshaychiruguri <73805429+Akshaychiruguri@users.noreply.github.com>
2021-07-19 11:49:16 +1000

    Update backend.tf

--- env/dev/us-east-1/backend.tf
+++ env/dev/us-east-1/backend.tf
@@ -5,11 +5,11 @@
 terraform {
   backend "s3" {
     # PER ENV: adapt these when copying!
-    key = "env/dev/us-east-1/state.tfstate"
+    key = "env/dev/ap-southeast-2/state.tfstate"
 
 
     # Common for security scripts
-    region         = "us-east-1"
+    region         = "ap-southeast-2"
     bucket         = "techautomationchallenge1-s3-terraform-remote-state"
     dynamodb_table = "techautomationchallenge1-dynamodb-terraform-remote-state-lock"
     encrypt        = true
